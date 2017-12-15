bd5eeeddc2146b3c9236e252a5ceb5b866a1638d
azarai <azarai@CowPower.local>
2017-12-15 14:32:04 +0100

    initial revision

---
+++ src/main/java/de/codeboje/kanbanapi/KanbanApiApplication.java
@@ -0,0 +1,12 @@
+package de.codeboje.kanbanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanApiApplication.class, args);
+	}
+}
